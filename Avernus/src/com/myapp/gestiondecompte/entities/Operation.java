b0e742af735e0fa6a9170008975be0e5c520b675
erikduhem <erikduhem@192.168.4.147>
2016-06-30 11:52:30 +0200

    ENTITES OPERATION RETRAIT VERSEMENT

--- Avernus/src/com/myapp/gestiondecompte/entities/Operation.java
+++ Avernus/src/com/myapp/gestiondecompte/entities/Operation.java
@@ -1,16 +1,110 @@
 package com.myapp.gestiondecompte.entities;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/*
+ * AUTEUR : Erik DUHEM
+ * VERSION : 1
+ * DATE : 30/06/2016
+ * 
+ */
 
 @Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 public class Operation {
 	
 	/*
-	 * 
+	 * ATTRIBUTS 
 	 */
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long idOperation;
+	private Date dateOperation;
+	private double montantOperation;
+	
+	/*
+	 * ASSOCIATIONS
+	 */
+	
+	@ManyToOne
+	@JoinColumn(name="employe")
+	private Employe employe;
+	
+	@ManyToOne
+	@JoinColumn(name="compte")
+	private Compte compte;
+
+	/*
+	 * GETTERS ET SETTERS 
+	 */
+	
+	public Long getIdOperation() {
+		return idOperation;
+	}
+
+	public void setIdOperation(Long idOperation) {
+		this.idOperation = idOperation;
+	}
+
+	public Date getDateOperation() {
+		return dateOperation;
+	}
+
+	public void setDateOperation(Date dateOperation) {
+		this.dateOperation = dateOperation;
+	}
+
+	public double getMontantOperation() {
+		return montantOperation;
+	}
+
+	public void setMontantOperation(double montantOperation) {
+		this.montantOperation = montantOperation;
+	}
+
+	public Employe getEmploye() {
+		return employe;
+	}
+
+	public void setEmploye(Employe employe) {
+		this.employe = employe;
+	}
+
+	public Compte getCompte() {
+		return compte;
+	}
+
+	public void setCompte(Compte compte) {
+		this.compte = compte;
+	}
+	
+	/*
+	 * CONSTRUCTEURS
+	 */
+	
+	public Operation() {
+		super();
+	}
+
+	public Operation(Date dateOperation, double montantOperation, Employe employe, Compte compte) {
+		super();
+		this.dateOperation = dateOperation;
+		this.montantOperation = montantOperation;
+		this.employe = employe;
+		this.compte = compte;
+	}
+	
+	
+	
 
 }
