2f7600082742a3600ea82d32480d6c079fdef2dd
erikduhem <erikduhem@192.168.4.147>
2016-06-30 12:20:52 +0200

    creation des tests unitaires

---
+++ Avernus/src/com/myapp/gestiondecompte/junit/TestEmploye.java
@@ -0,0 +1,14 @@
+package com.myapp.gestiondecompte.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEmploye {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
