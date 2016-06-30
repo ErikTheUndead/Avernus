9bc818ddf7006c1c1716eda62bf51a5926b99380
erikduhem <erikduhem@192.168.4.147>
2016-06-30 10:51:18 +0200

    creation entites seconde partie

---
+++ Avernus/src/com/myapp/gestiondecompte/entities/CompteCourant.java
@@ -0,0 +1,5 @@
+package com.myapp.gestiondecompte.entities;
+
+public class CompteCourant extends Compte{
+
+}
---
+++ Avernus/src/com/myapp/gestiondecompte/entities/Groupe.java
@@ -0,0 +1,5 @@
+package com.myapp.gestiondecompte.entities;
+
+public class Groupe {
+
+}
