acb6d3d06d2d1e5b134243cbffcbba7f5c198cef
thomasbernard78 <thomas.bernard66@hotmail.fr>
2016-06-30 14:57:02 +0200

    Commit DaoClient et impl

--- Avernus/src/com/myapp/gestiondecompte/dao/client/DaoClientImpl.java
+++ Avernus/src/com/myapp/gestiondecompte/dao/client/DaoClientImpl.java
@@ -1,5 +1,106 @@
 package com.myapp.gestiondecompte.dao.client;
 
-public class DaoClientImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.myapp.gestiondecompte.dao.singleton.Singleton;
+import com.myapp.gestiondecompte.entities.Client;
+
+/*
+ *  author : BERNARD Thomas
+ *   class : DaoClientImpl
+ *    date : 30/06/2016
+ * version : 1.0
+ * 
+ */
+
+public class DaoClientImpl implements IDaoClient {
+
+	Logger logger = Logger.getLogger("DaoImpl") ;
+	private SessionFactory sf = Singleton.getSf();
+	
+	
+	@Override
+	public Client addClient(Client c) {
+	
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.saveOrUpdate(c);
+		session.getTransaction().commit();
+		session.close();
+		logger.info("le client "+c.getNomClient()+ "  avec l'id "+c.getIdClient()+" a bien ete enregistrer");
+		return c;
+	}
+
+	@Override
+	public void deleteClient(Long idC) {
+		
+		Session session = sf.openSession();
+
+		session.beginTransaction();
+		Client c = session.get(Client.class, idC);
+		session.delete(c);
+		logger.info("on supprime le client d'id : " + c.getIdClient());
+
+		session.getTransaction().commit();
+		session.close();
+	}
+		
+	
+
+	@Override
+	public Client updateClient(Client c) {
+		
+		logger.info("on modifie l'etudiant  d'id : "+ c.getIdClient());
+		Session session = sf.openSession();
+		session.beginTransaction();
+
+		session.update(c);
+		session.getTransaction().commit();
+		session.close();
+		return c;
+	}
+
+	@Override
+	public List<Client> getClients() {
+		
+		logger.info("on recuper la liste des clients");
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
+		String hql ="FROM Client ";
+		Query rep = session.createQuery(hql);
+		List<Client> list = new ArrayList<>();
+		list = rep.list();
+		session.getTransaction().commit();
+		session.close();
+		
+		return list;
+		
+		
+	}
+
+	@Override
+	public List<Client> getClientsByMc(String mc) {
+		logger.info("on recuper une liste d'etudiants par mot cle");
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
+		String hql ="FROM Client Where nomClient  LIKE ? OR prenomClient LIKE ?";
+		Query rep = session.createQuery(hql);
+		rep.setString(0, "%"+mc+"%");
+		rep.setString(1, "%"+mc+"%");
+		List<Client> list = new ArrayList<>();
+		list = rep.list();
+		session.getTransaction().commit();
+		session.close();
+		
+		return list;
+	}
 
 }
--- Avernus/src/com/myapp/gestiondecompte/dao/client/IDaoClient.java
+++ Avernus/src/com/myapp/gestiondecompte/dao/client/IDaoClient.java
@@ -1,5 +1,21 @@
 package com.myapp.gestiondecompte.dao.client;
 
+import java.util.List;
+
+import com.myapp.gestiondecompte.entities.Client;
+
+/*
+ *  author : BERNARD Thomas
+ *   class : IDaoClient
+ *    date : 30/06/2016
+ * version : 1.0
+ * 
+ */
 public interface IDaoClient {
 
+		public Client addClient(Client c);
+		public void deleteClient(Long idC);
+		public Client updateClient(Client c);
+		public List<Client> getClients();
+		public List<Client> getClientsByMc(String mc);
 }
