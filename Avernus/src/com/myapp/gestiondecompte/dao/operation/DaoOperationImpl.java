5d917be7134c0bb8c0ccf55d4a6c28d4ac9162aa
erikduhem <erikduhem@192.168.4.147>
2016-06-30 14:45:30 +0200

    Interface et Impl de compte et operation

--- Avernus/src/com/myapp/gestiondecompte/dao/operation/DaoOperationImpl.java
+++ Avernus/src/com/myapp/gestiondecompte/dao/operation/DaoOperationImpl.java
@@ -1,5 +1,36 @@
 package com.myapp.gestiondecompte.dao.operation;
 
-public class DaoOperationImpl {
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
 
+import com.myapp.gestiondecompte.dao.singleton.Singleton;
+import com.myapp.gestiondecompte.entities.Operation;
+
+public class DaoOperationImpl implements IDaoOperation{
+	
+	/*
+	 * ATTRIBUTS
+	 */
+	private Logger logger = Logger.getLogger("DaoOperationImpl");
+	private SessionFactory sf = Singleton.getSf();
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.myapp.gestiondecompte.dao.operation.IDaoOperation#addOperation(com.myapp.gestiondecompte.entities.Operation)
+	 */
+	
+	@Override
+	public Operation addOperation(Operation op) {
+		Session ss = sf.openSession();
+		ss.beginTransaction();
+		ss.save(op);
+		ss.getTransaction().commit();
+		ss.close();
+		logger.info("l'operation "+op.getIdOperation()+" á bien été enregistré");
+		return op;
+	}
+
+	
+	
 }
